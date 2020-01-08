911b09e87bafc63f786fcafe063e2d480a71fd67
priyanka-cr-dxc <priyanka.cr-dxc@hpe.com>
2020-01-08 15:00:02 +0530

    first commit

---
+++ src/main/scala/Firstprogram.scala
@@ -0,0 +1,12 @@
+object Firstprogram {
+
+  def main(args: Array[String]): Unit = {
+    var x: Int = 30
+    if (x > 20) {
+      println("the statement is correct")
+    }
+    else {
+      println("the statement is wrong")
+    }
+  }
+}
